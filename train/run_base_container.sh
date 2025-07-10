dee2e167b19273379635c2c6fb6e1bb898b10f9c
ahaampo5 <ahaampo5@gmail.com>
2025-07-10 17:50:42 +0900

    [Add] run base container

---
+++ train/run_base_container.sh
@@ -0,0 +1,18 @@
+
+NUM_GPUS=all
+WORKSPACE=/mnt/data01/workspace/jckim:/root/workspace
+HUGGINGFACE_CACHE=/mnt/data01/huggingface/cache
+CONTAINER_NAME=server_jckim
+SHARED_MEMORY=4G
+DOCKER_IMAGE=
+
+docker run -dit \
+	--net host \
+	--gpus $NUM_GPUS \
+	-v $WORKSPACE:/root/workspace \
+	-v $HUGGINGFACE_CACHE:/root/.cache/huggingface \
+	--name $CONTAINER_NAME \
+	--shm-size $SHARED_MEMORY \
+	$DOCKER_IMAGE
+
+
